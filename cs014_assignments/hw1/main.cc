31d7d3f445b477ad69eb9ae9d647d4356aea5f1f
jlai0171 <jlai017@student.ucr.edu>
2015-04-15 22:51:15 +0000

    second

---
+++ cs014_assignments/hw1/main.cc
@@ -0,0 +1,14 @@
+#include "MyList.H"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    MyList l;
+    
+    l.push_back(3);
+    l.push_back(6);
+    l.push_back(7);
+    return 0;
+}
