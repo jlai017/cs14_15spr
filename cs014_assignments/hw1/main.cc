57d364457605740070ee44c6015664a852605496
jlai0171 <jlai017@student.ucr.edu>
2015-04-16 04:04:36 +0000

    third

--- cs014_assignments/hw1/main.cc
+++ cs014_assignments/hw1/main.cc
@@ -5,10 +5,26 @@ using namespace std;
 
 int main() {
     
-    MyList l;
+    MyList l = MyList();
+    string n = "something";
+    MyList s = MyList(n);
     
-    l.push_back(3);
-    l.push_back(6);
-    l.push_back(7);
+    l.push_back('s');
+    l.push_back('t');
+    l.push_back('u');
+    l.push_back('v');
+    l.push_back('w');
+    l.push_back('x');
+    l.push_back('y');
+    l.push_back('z');
+    
+    l.print();
+    //l.swap(1, 4);
+    l.reverse();
+    l.print();
+    l.insert_at_pos(1, 'j');
+    l.print();
+    cout << l.find('v') << endl;
+    s.print();
     return 0;
 }
